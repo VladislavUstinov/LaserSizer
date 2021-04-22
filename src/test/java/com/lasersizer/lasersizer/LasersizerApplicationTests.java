35dd34d9c21d34cd7a5594852e05c5f33b3683dd
VladislavUstinov <vladustinov90@gmail.com>
2021-04-22 17:35:17 +0300

    Commiting newly created project1

---
+++ src/test/java/com/lasersizer/lasersizer/LasersizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lasersizer.lasersizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LasersizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
