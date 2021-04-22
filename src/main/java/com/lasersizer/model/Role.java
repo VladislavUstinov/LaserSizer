31627630fc1918a20d0590b55171ad2738e69435
VladislavUstinov <vladustinov90@gmail.com>
2021-04-22 18:54:08 +0300

    Added POJOs User and Role to start working with Spring Security. Closes #2

---
+++ src/main/java/com/lasersizer/model/Role.java
@@ -0,0 +1,41 @@
+package com.lasersizer.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "role_id")
+public class Role implements GrantedAuthority {
+
+    @Id
+    private Long id;
+    private String name;
+
+    @Transient
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    public Role() {
+    }
+
+    public Role(Long id) {
+        this.id = id;
+    }
+
+    public Role(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String getAuthority() {
+        return getName();
+    }
+}
